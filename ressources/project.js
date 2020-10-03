585f29c3c7bb27c80296f8ad794a63caa2bafb32
WU_PIERRE <pierre.wu@edu.univ-paris13.fr>
2020-10-03 04:36:50 +0200

    sort checkbox

--- ressources/project.js
+++ ressources/project.js
@@ -1,36 +1,78 @@
-let s_btn = document.getElementsByClassName('s_btn');
-let s_btn_length = s_btn.length; 
+let c_btn = document.getElementsByClassName('c_btn');
 
-const img_src = "../ressources/display/sq.svg";
-for(let i = 0; i <s_btn_length; i++){
-    let dom =  document.getElementsByClassName('s_btn')[i];
-    let DOM_img = document.createElement("img");
-    DOM_img.src = img_src;  
-    document.getElementsByClassName('s_btn')[i].appendChild(DOM_img);
+const uncheckbox = "../ressources/display/uncheckbox.png";
+const checkbox = "../ressources/display/checkbox.png";
 
-} 
+//init all unchecked img
+for(let i = 0; i < c_btn.length; i++){
+    c_btn[i].src = uncheckbox;  
+}
+
+//takes param and remove dupe
+let pl_tab = pl_init("pl");
+
+function pl_init(param){
+    let p = url_get(param);
+    if(p == null){
+        return [];
+    }
+    else{
+        //remove dupes
+        let p_split = p.split("+");
+        let new_tab = [];
+        p_split.forEach(e => {
+            if(!new_tab.includes(e)){
+                new_tab.push(e);
+                checkb(e);
+            }
+        });
+        //update no dupes param
+        n_url= window.location.href.split('?')[0] + "?"+param+"="+ new_tab.join("+");
+        window.history.replaceState( {} , '', n_url );
+        return new_tab;
+    }
+ }
+
 
 function btn_click(pl){
-    insertParam("pl", pl);
+    if(!pl_tab.includes(pl)){
+        insertParam("pl", pl);
+        checkb(pl);
+    }
+    else{
+        pop_lang("pl",pl);
+        uncheckb(pl);
+    }
     
 }
 
 
 function insertParam(key, value) {
-    console.log()
     if(url_get(key) == null){
         let n_url = window.location.href +"?"+ key + "="+ value;
         window.history.replaceState( {} , '', n_url );
+        pl_tab.push(value);
     }
     else{// only adapted to one parameter key
         let n_url= window.location.href +"+"+ value;
         window.history.replaceState( {} , '', n_url );
-        console.log(pl_tab);
         console.log(url_get("pl"));
+        pl_tab.push(value);
     }
-    
-   
+}
 
+function pop_lang(key, value){
+    let n_url;
+    pl_tab.pop(value);
+    let url_main =  window.location.href.split('?')[0];
+    if(pl_tab.length == 0){
+        n_url = url_main;
+    }
+    else{
+        n_url= url_main + "?"+key+"="+ pl_tab.join("+");
+    }
+    window.history.replaceState( {} , '', n_url );
+    
 }
 
 function url_get(param) {
@@ -45,4 +87,12 @@ function url_get(param) {
 		return vars[param] ? vars[param] : null;	
     }
 	return vars;
+}
+
+function checkb(lang){
+    document.getElementById(lang).src = checkbox;
+}
+
+function uncheckb(lang){
+    document.getElementById(lang).src = uncheckbox;
 }
